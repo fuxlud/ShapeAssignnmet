7f6cf1690f028060f4528bef0f9282a516328f8a
Luda Fux <luda.fux@similarweb.com>
2022-09-23 22:49:04 +0300

    testing on favManager

--- AssignmentTests/FavoritePictures/FavoriteManagerTests.swift
+++ AssignmentTests/FavoritePictures/FavoriteManagerTests.swift
@@ -5,19 +5,18 @@ final class FavoriteManagerTests: XCTestCase {
 
     var sut: FavoritesManager!
     let localPersistenceSpy = LocalPersistenceSpy()
+    let imageDetails = ImageDetails(url: URL(string: "www.shape.dk")!)
+    let breedName = "shuki"
     
     override func setUpWithError() throws {
         sut = FavoritesManager(localPersistence: localPersistenceSpy)
+        imageDetails.breedName = breedName
     }
 
-    func testLikingSavesImageToFavoritesByBreedToLocalPersistance() async throws  {
-        let imageDetails = ImageDetails(url: URL(string: "www.shape.dk")!)
-        imageDetails.breedName = "shuki"
-        
+    func testLikingSavesImageToFavoritesByBreedToLocalPersistance() async throws {
         await sut.like(imageDetails: imageDetails)
-        
-        let favoritesByBreed = loadFavoritesByBreedFromLocalPersistanceSpy()
-        XCTAssertEqual(favoritesByBreed.keys.first, "shuki")
+        let favoritesByBreedRecorded = loadFavoritesByBreedFromLocalPersistanceSpy()
+        XCTAssertEqual(favoritesByBreedRecorded.keys.first, "shuki")
     }
 
     private func loadFavoritesByBreedFromLocalPersistanceSpy() -> [String: [ImageDetails]] {
@@ -25,6 +24,21 @@ final class FavoriteManagerTests: XCTestCase {
         let decoder = JSONDecoder()
         let favoritesByBreed = try! decoder.decode([String: [ImageDetails]].self, from: favoritesByBreedData)
         return favoritesByBreed
+    }
+    
+    func testLikingSavesImageTo() async throws {
+        await sut.like(imageDetails: imageDetails)
+        
+        let allFavoritesRecorded = loadAllFavoritesFromLocalPersistanceSpy()
+        let imageRecorded = allFavoritesRecorded.first!
+        XCTAssertEqual(imageRecorded, imageDetails)
+    }
+    
+    private func loadAllFavoritesFromLocalPersistanceSpy() -> [ImageDetails] {
+        let allFavoritesData = localPersistenceSpy.object(forKey: "allFavorites") as! Data
+        let decoder = JSONDecoder()
+        let allFavorites = try! decoder.decode([ImageDetails].self, from: allFavoritesData)
+        return allFavorites
     }
 
 }
